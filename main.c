7f2b0103430062fc9fbe0ed09a270043326c67c5
Mohab Sharafeldin <100772670+Mohab021@users.noreply.github.com>
2022-03-12 13:30:31 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+
+#define F_CPU 8000000UL
+#include <util/delay.h>
+#include <avr/io.h>
+#include "AVR_Ports.h"
+#include "AVR_LCD_Declarations.h"
+
+int main(void)
+{
+	LCD_Initialize();
+	LCD_Print("Mohab");
+}
+
+
